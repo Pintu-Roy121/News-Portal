48b79c1c1fd636269fab3c520eef435f300ba933
Pintu-Roy121 <roypintu121@gmail.com>
2022-09-02 23:50:58 +0600

    menu list added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('js file is added');
+const loadCatagory = async () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    const res = await fetch(url)
+    const data = await res.json()
+    return data;
+}
+
+const showCatagory = async () => {
+    const data = await loadCatagory()
+    // console.log(data.data.news_category)
+    const categories = data.data.news_category;
+    // console.log(categories)
+
+    const menuItems = document.getElementById('categories-items');
+    categories.forEach(catagory => {
+        // console.log(catagory)
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <a>${catagory.category_name}</a>
+        `;
+        menuItems.appendChild(li);
+
+    });
+
+}
+
+showCatagory();
